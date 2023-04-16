676dc64d3f4f071f7214cb953be13044b5179b9d
Facundo Aletto <facundoaletto95@gmail.com>
2023-04-16 12:18:02 -0300

    Ejercicio 1 Parte 1

--- Problema1.js
+++ Problema1.js
@@ -1,4 +1,4 @@
-const USUARIO_NOMBRE_REGISTRADO_A = "pepe";
+/* const USUARIO_NOMBRE_REGISTRADO_A = "pepe";
 const USUARIO_CONTRASENIA_REGISTRADA_A = "asd123";
 const USUARIO_NOMBRE_REGISTRADO_B = "pablo";
 const USUARIO_CONTRASENIA_REGISTRADA_B = "1234.pablo";
@@ -7,6 +7,18 @@ let usuarioNombreIngresado = prompt("Ingrese nombre de usuario");
 let usuarioContraseniaIngresada = prompt("Ingrese su contraseña");
 
 if (((usuarioNombreIngresado == USUARIO_NOMBRE_REGISTRADO_A) || (usuarioNombreIngresado == USUARIO_NOMBRE_REGISTRADO_B)) && ((usuarioContraseniaIngresada == USUARIO_CONTRASENIA_REGISTRADA_A) || (usuarioContraseniaIngresada == USUARIO_CONTRASENIA_REGISTRADA_B))) {
+    document.write("Bienvenido a su cuenta")
+}else{
+    document.write("Usuario y contraseña incorrecta")
+}; */
+
+const USUARIO_NOMBRE_REGISTRADO = "pepe";
+const USUARIO_CONTRASENIA_REGISTRADA = "asd123";
+
+let usuarioNombreIngresado = prompt("Ingrese nombre de usuario");
+let usuarioContraseniaIngresada = prompt("Ingrese su contraseña");
+
+if ((usuarioNombreIngresado == USUARIO_NOMBRE_REGISTRADO) && (usuarioContraseniaIngresada == USUARIO_CONTRASENIA_REGISTRADA)) {
     document.write("Bienvenido a su cuenta")
 }else{
     document.write("Usuario y contraseña incorrecta")
